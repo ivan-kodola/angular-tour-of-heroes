ab930ef4c3348d8b40ca1927641285654735ea0a
ivan-kodola <ivan.kodola@gmail.com>
2018-03-25 19:04:16 +0300

    feature/TOH-6

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -38,10 +38,39 @@ export class HeroService {
 	  );
 	}
 
-	private log(message: string) {
-	  this.messageService.add('HeroService: ' + message);
+	updateHero (hero: Hero): Observable<any> {
+	  return this.http.put(this.heroesUrl, hero, httpOptions).pipe(
+	    tap(_ => this.log(`updated hero id=${hero.id}`)),
+	    catchError(this.handleError<any>('updateHero'))
+	  );
 	}
 
+	addHero (hero: Hero): Observable<Hero> {
+	  return this.http.post<Hero>(this.heroesUrl, hero, httpOptions).pipe(
+	    tap((hero: Hero) => this.log(`added hero w/ id=${hero.id}`)),
+	    catchError(this.handleError<Hero>('addHero'))
+	  );
+	}
+
+	deleteHero (hero: Hero | number): Observable<Hero> {
+	  const id = typeof hero === 'number' ? hero : hero.id;
+	  const url = `${this.heroesUrl}/${id}`;
+
+	  return this.http.delete<Hero>(url, httpOptions).pipe(
+	    tap(_ => this.log(`deleted hero id=${id}`)),
+	    catchError(this.handleError<Hero>('deleteHero'))
+	  );
+	}
+
+	searchHeroes(term: string): Observable<Hero[]> {
+	  if (!term.trim()) {
+	    return of([]);
+	  }
+	  return this.http.get<Hero[]>(`api/heroes/?name=${term}`).pipe(
+	    tap(_ => this.log(`found heroes matching "${term}"`)),
+	    catchError(this.handleError<Hero[]>('searchHeroes', []))
+	  );
+	}
 
 	private handleError<T> (operation = 'operation', result?: T) {
 	  return (error: any): Observable<T> => {
@@ -54,10 +83,7 @@ export class HeroService {
 	  };
 	}
 
-	updateHero (hero: Hero): Observable<any> {
-	  return this.http.put(this.heroesUrl, hero, httpOptions).pipe(
-	    tap(_ => this.log(`updated hero id=${hero.id}`)),
-	    catchError(this.handleError<any>('updateHero'))
-	  );
+	private log(message: string) {
+	  this.messageService.add('HeroService: ' + message);
 	}
 }
